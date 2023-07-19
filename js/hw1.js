5c824e16e8de8edd2ad45138f36439d796bea9e1
Anade1 <104677335+Anade1@users.noreply.github.com>
2023-07-19 11:48:36 +0300

    commit

---
+++ js/hw1.js
@@ -0,0 +1,58 @@
+// Завдання 1
+
+let str = 'Привіт';
+let num = 123;
+let flag = true;
+let txt = 'true';
+
+console.log(typeof str);  // string
+console.log(typeof num);  // number
+console.log(typeof flag);  // boolean
+console.log(typeof txt);  // string
+
+
+// Завдання 2
+
+let name = 'John'; // string
+let age = 29; // number
+let isDay = true; // boolean
+let height = 186; // number
+let fruit = 'banana'; // string
+const pi = 3.14; // number
+const city = 'Odessa'; // string
+const achieve = false; // boolean
+const grade = 12; // array
+const car = 'Toyota'; // string
+
+console.log(name);
+console.log(age);
+console.log(isDay);
+console.log(height);
+console.log(fruit);
+console.log(pi);
+console.log(city);
+console.log(achieve);
+console.log(grade);
+console.log(car);
+
+
+// Завдання 3
+
+let user = prompt("Ryan Gosling");
+console.log(user);
+
+
+// Завдання 4
+
+let myAge = 15;
+let friendAge = 14;
+
+let sum = myAge + friendAge;
+alert(sum);
+
+
+// Завданяя 5
+
+let group = "FE 1y_5";
+
+console.log("Я навчаюся в GoITeens в групі: ", group);
---
+++ js/numbers.js
@@ -0,0 +1,7 @@
+const a = 1
+const b = 7
+const c = "1"
+
+console.log(b % a);
+
+console.log(c == a)
