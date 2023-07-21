0b081e73d510c2e477c87a29320d0df644959648
Anade1 <104677335+Anade1@users.noreply.github.com>
2023-07-21 10:55:14 +0300

    commit

---
+++ js/hw3.js
@@ -0,0 +1,44 @@
+// Завдання 1
+
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+
+const totalPrice = pricePerItem * orderedQuantity;
+console.log(totalPrice);
+
+
+// Завдання 2
+
+const age = 31;
+
+const passed = age >= 18;
+
+console.log(passed);
+
+
+// Завдання 3
+
+const SAVED_PASSWORD = "qwerty"; 
+const password = prompt("Пароль на базу:"); 
+
+const isMatch = password === SAVED_PASSWORD;
+
+console.log(isMatch);
+
+
+// Завдання 4
+
+let a = 5;
+let b = 10;
+let c = 15;
+let d = 20;
+
+a += 2; 
+b -= 4; 
+c *= 3;
+d /= 10;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
