d2df4d15cdfeff9c1deba2df136c7a70296dd4ad
Anade1 <104677335+Anade1@users.noreply.github.com>
2023-09-27 19:26:17 +0300

    commit

---
+++ js/hw9.js
@@ -0,0 +1,82 @@
+// Завдання 1
+
+// let number = 1;
+
+// while (number <= 10) {
+
+//     console.log(number);
+
+//     number+=;
+// }
+
+
+
+
+
+
+// Завдання 2
+
+
+// for (let number = 2; number <= 20; number++) {
+//     if (number % 2 !== 0) {
+//         continue;
+//     }
+
+
+//     console.log(number); 
+// }
+
+
+
+
+
+// Завдання 3
+
+// for (let number = 1; number <= 10; number++) {
+
+//     let result = 7 * number;
+//     console.log(result);
+
+// }
+
+
+
+
+
+
+// Завдання 4
+
+// const numbers = [1, 2, 3, ]
+
+
+
+
+
+
+// Завдання 5
+
+// for (let number = 1; number <= 10; number++) {
+//     if (number === 7) {
+//         break;
+//     }
+
+//     console.log(number);
+// }
+
+
+
+
+// Завдання 6
+
+
+let n = 30;
+
+for (let number = 1; number < n; number++) {
+    if (number >= n) {
+        break;
+    }
+
+    console.log(number)
+}
+
+
