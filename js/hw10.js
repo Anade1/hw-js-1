e2405486525059901a181d48ff856b830bcf0df6
Anade1 <104677335+Anade1@users.noreply.github.com>
2023-10-04 19:32:18 +0300

    commit

---
+++ js/hw10.js
@@ -0,0 +1,44 @@
+// Завдання 1
+
+const company = ['Pepsi', 'Fanta', 'Sprite'];
+company[1] = '10';
+console.log(company)
+
+
+// Завдання 2
+
+const number = [
+    ['1', '2', '3'],
+
+    ['4', '5', '6'],
+
+    ['7', '8', '9']
+];
+
+number.push('Google', 'Youtube', 'Discord');
+console.log(number);
+
+
+// Завдання 3
+
+let total = [10, 20, 50, 40, 200];
+let sum = 0;     
+// Зробив через цикл for.
+for (let i = 0; i < total.length; i++) {
+    sum += total[i];
+}
+console.log(sum);
+
+
+
+
+
+// Завдання 4
+
+let numbers = [1, 2, 3, 4, 5];
+
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
